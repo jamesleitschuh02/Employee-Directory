637bfbd603e1cb6b21fdc092148358a4e902eb16
James Leitschuh <james.leitschuh02@gmail.com>
2021-02-27 14:58:02 -0600

    succesfully renders page and employee number

--- employeeapp/src/components/SearchForm/index.js
+++ employeeapp/src/components/SearchForm/index.js
@@ -7,7 +7,11 @@ function SearchForm(props) {
             <label htmlFor="language">Number</label>
             <input
                 value={props.search}
+                onChange={props.handleInputChange}
             />
+            <button type="submit" onClick={props.handleFormSubmit} className="btn btn-success">
+            Search
+        </button>
         </div>
     </form>
     );
--- employeeapp/src/pages/Search/index.js
+++ employeeapp/src/pages/Search/index.js
@@ -1,33 +1,45 @@
 /* eslint-disable no-unused-vars */
-import React, { useState } from "react";
-// import { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import API from "../../utils/API";
 import SearchForm from "../../components/SearchForm/index";
 
 function Search(){
-    const [search, setSearch] = useState("20");
+    const [search, setSearch] = useState(20);
     const [results, setResults] = useState([]);
 
-    // useEffect(() => {
-    //     if (!search) {
-    //         return;
-    //     }
-    // })
+    const handleInputChange = event => {
+        setSearch(event.target.value);
+    };
 
     const handleFormSubmit = event => {
         event.preventDefault();
         API.employeeSearch(search)
         .then(res => {
-            setResults(res);
+            console.log(res.data.results);
+            setResults(res.data.results);
         })
     };
+    
+    useEffect(() => {
+        console.log("Our new list is ", results);
+    },[results]);
 
     return (
-        <div>
-            <SearchForm
-                results={search}
-            />
-        </div>
+        <>
+            <div>
+                <SearchForm
+                    results={search}
+                    handleFormSubmit={handleFormSubmit}
+                    handleInputChange={handleInputChange}
+                />
+            </div>
+            <div>
+                {results.map((index) => {
+                    return (<p key={index.email}>{index.name.first}</p>);
+                    // return (<Table key={index.email} name={index.name.first} />)
+                })}
+            </div>
+        </>
     );
 }
 
